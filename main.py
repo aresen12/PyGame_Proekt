82100feb752cf1acd9f6413618dd2d61e95ecb49
aresen12 <Ares.Ares.Ars@yandex.ru>
2024-01-04 16:50:25 +0300

    препятствия движуться столкнование

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import random
 import os
 import sys
 
+size = 1000, 600
+pygame.init()
+screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
 obstacle = []
 
@@ -27,6 +30,7 @@ class Car(pygame.sprite.Sprite):
     def __init__(self, *grop):
         super().__init__(*grop)
         self.image = pygame.transform.scale(load_image("car.png"), (100, 150))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = 230
         self.rect.y = 370
@@ -72,18 +76,34 @@ class Obstacle(pygame.sprite.Sprite):
         super().__init__(*grop)
         self.image = pygame.transform.scale(load_image("police.png"), (100, 150))
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = random.randint(230, 230 + 120 * 4)
         self.rect.y = random.randint(40, 500)
+        self.y = self.rect.y
 
     def update(self):
         pass
+def wait():
+    global all_sprites
+    all_sprites = pygame.sprite.Group()
+    obstacle.clear()
+    plaing_not = True
+    while plaing_not:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                plaing_not = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                main()
 
 
 def main():
-    size = 1000, 600
-    pygame.init()
-    screen = pygame.display.set_mode(size)
+    tick = pygame.time.Clock()
     car = Car()
+    screen.fill("black")
+    for ob in range(2):
+        obstacle.append(Obstacle())
+        all_sprites.add(obstacle[-1])
     all_sprites.add(car)
     all_sprites.draw(screen)
     board = Board(size[0], size[1])
@@ -94,6 +114,26 @@ def main():
                 running = False
             if event.type == 768:
                 car.update(event)
+        d = tick.tick() / 1000
+
+        for ob in obstacle:
+            ob: Obstacle
+            if not pygame.sprite.collide_mask(ob, car):
+                ob.y += d * 50
+                ob.rect.y = int(ob.y)
+            else:
+                font = pygame.font.Font(None, 50)
+                text = font.render("Столкновение!", True, "red")
+                text_x = size[0] // 2 - text.get_width() // 2
+                text_y = size[1] // 2 - text.get_height() // 2
+                text_w = text.get_width()
+                text_h = text.get_height()
+                screen.blit(text, (text_x, text_y))
+                pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                                       text_w + 20, text_h + 20), 1)
+                pygame.display.update()
+                wait()
+                return None
         board.render(screen)
         pygame.display.update()
     pygame.quit()
